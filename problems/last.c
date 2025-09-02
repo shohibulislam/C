963350431a2893df38d719c564d50d196ef6ad18
Shohibul <shohibul602@gmail.com>
2025-09-02 21:25:55 +0600

    added array end-insertion C program

--- problems/last.c
+++ problems/last.c
@@ -1,6 +1,8 @@
+// a C program to insert an element at the end of an array.
+
 #include<stdio.h>
 
-int last(int );
+void last(int a[], int n);
 
 int main(){
     int n, x;
@@ -12,11 +14,16 @@ int main(){
         scanf("%d",&a[i]);
     }
 
+    last(a, n);
+
+}
+
+void last(int a[], int n){
     printf("Entered array-\n");
     for(int i=0; i<n; i++){
         printf("%d ",a[i]);
     }
-
+    int x;
     printf("\nEnter number to insert at the end of the array: ");
     scanf("%d",&x);
 
