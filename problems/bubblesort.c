31b2d04a5c71ef5f1b300ef058ee17fa502f8ad8
Shohibul <shohibul602@gmail.com>
2025-09-04 18:54:54 +0600

    added bubble sort C program

---
+++ problems/bubblesort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d",&a[i]);
+    }
+
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(a[j] > a[j+1]){
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+
+    int count = 0;
+    for(int i=0; i<n; i++){
+        if(a[i] != a[i+1]){
+            count++;
+        }
+    }
+    printf("%d", count);
+}
