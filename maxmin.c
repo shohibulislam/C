e0000b8f4eacc5195f7750d6e028d69a50366478
Shohibul <shohibul602@gmail.com>
2025-09-05 16:54:10 +0600

    added program to find max and min in array

--- maxmin.c
+++ maxmin.c
@@ -1,19 +1,23 @@
+//a program to find max and min in array
 #include<stdio.h>
 int main(){
-    int a[5];
+    int n;
+    printf("enter size of array: ");
+    scanf("%d",&n);
+    int a[n];
     printf("Enter array.\n");
-    for(int i=0; i<5; i++){
+    for(int i=0; i<n; i++){
         printf("Index number %d of array:",i);
         scanf("%d",&a[i]);
     }
 
     printf("Entered array \n");
-    for(int i=0; i<5; i++){
+    for(int i=0; i<n; i++){
         printf("%d ",a[i]);
     }
     
     int max=a[0];
-    for(int i=1; i<5; i++){
+    for(int i=1; i<n; i++){
         if(max<a[i]){
             max=a[i];
         }
@@ -21,10 +25,11 @@ int main(){
     printf("\nMaximum number is %d\n",max);
 
     int min=a[0];
-    for(int i=1; i<5; i++){
+    for(int i=1; i<n; i++){
         if(min>a[i]){
             min=a[i];
         }
     }
     printf("Minimum number is %d",min);
+
 }
