3a36cd9fc2428a8a9b9f27b3907adfd010edfdaf
Shohibul <shohibul602@gmail.com>
2025-09-05 07:25:29 +0600

    added custom and libray method of salting function

---
+++ salting.c
@@ -0,0 +1,43 @@
+//manual method of salting
+
+#include<stdio.h>
+#include<string.h>
+
+void salt(char a[]);
+
+int main(){
+    char a[100];
+    printf("Enter Password: ");
+    scanf("%s",a);
+    salt(a);
+}
+
+void salt(char a[]){
+    int n = strlen(a);
+    char x[] ="123";
+    for(int i=0; x[i] != '\0'; i++){
+        a[n+i] = x[i];
+    }
+
+    a[n + strlen(x)] = '\0';
+
+    printf("Salted Form: ");
+    for(int i=0; a[i] != '\0'; i++){
+        printf("%c",a[i]);
+    }
+}
+
+//libary method
+
+// #include<stdio.h>
+// #include<string.h>
+
+// int main(){
+//     char a[100];
+//     printf("Enter Password: ");
+//     scanf("%s",a);
+//     char x[] = "123";
+//     strcat(a , x);
+//     printf("Salting Form: ");
+//     printf("%s",a);
+// }
